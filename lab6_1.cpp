e2c76cd498f6d85c39a58fc44d394496fda2b799
Ixac <ixacarrrows@gmail.com>
2021-11-26 16:39:33 +0700

    labข้อ2

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+int a;
+int ku=0;
+int ki=0;
+while(a!=0){
+cout << "Enter an interger: "; 
+cin >> a;
+    if(a%2==0){
+        ku++;
+        if(a==0){
+            ku--;
+        }
+    }
+    else{
+        ki++;
+    }
+}
+cout << "#Even numbers = "<< ku <<"\n";
+cout << "#Odd numbers = "<< ki;
+
+  return 0;
+
 }
